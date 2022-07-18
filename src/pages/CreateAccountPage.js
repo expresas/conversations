7a32ab342c1a87e1abb5324473da7aab99017fea
expresas <expresas@gmail.com>
2022-07-18 06:40:31 +0300

    minnor update

--- src/pages/CreateAccountPage.js
+++ src/pages/CreateAccountPage.js
@@ -62,7 +62,6 @@ const CreateAccountPage = () => {
       username: usernameRef.current.value,
       password: pass1Ref.current.value, 
       image: 'https://www.kindpng.com/picc/m/24-248253_user-profile-default-image-png-clipart-png-download.png',
-      chats: [],
       banned: [],
     }
     
